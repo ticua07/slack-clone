41b2bf69c34c1e975931a18b6a339d3f000c48a9
Ticua <ticua@ticua.ar>
2024-01-25 15:08:19 -0300

    Added supabase types

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,8 +1,9 @@
+import { Database } from "@/types/supabase";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
