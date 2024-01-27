a1fcda0928c123024bb4d89014fb846edb88b20e
Ticua <ticua@ticua.ar>
2024-01-27 19:30:36 -0300

    Update supabase types and utils

--- utils/supabase/cache.ts
+++ utils/supabase/cache.ts
@@ -1,7 +1,11 @@
 import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-export const createFetch =
+// a cache of 30s should be enough to when messages are loaded
+// we don't repeat the same user info request twice
+const REVALIDATE_TIME = 30
+
+const createFetch =
     (options: Pick<RequestInit, "next" | "cache">) =>
         (url: RequestInfo | URL, init?: RequestInit) => {
             return fetch(url, {
@@ -10,15 +14,8 @@ export const createFetch =
             });
         };
 
-
-// a cache of 30s should be enough to when messages are loaded
-// we don't repeat the same user info request twice
-
-const REVALIDATE_TIME = 30
-
 // this clients runs on the anon_key only
 // made specifically for the users/ route
-
 export const createCacheClient = () =>
     createClient<Database>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
