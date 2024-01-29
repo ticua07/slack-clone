9c4bb111f5b2d79de4b913d0965d4e68ac5fc65e
Ticua <ticua@ticua.ar>
2024-01-28 22:10:20 -0300

    Add message fetching

--- app/components/Messages.tsx
+++ app/components/Messages.tsx
@@ -10,24 +10,26 @@ export default function Messages() {
     const [messages, setMessages] = useState<CombinedMessage[]>([]);
 
     useEffect(() => {
-        // no channel has been selected yet.
+        setMessages([]); // Set messages to empty array when starting the fetch
         if (context?.currentChannel?.channel_id === undefined) { return; }
-        fetch(`/api/messages?channel_id=${context?.currentChannel?.channel_id}`).then(async res => {
-            const json = await res.json()
-            console.log(json)
-            setMessages(json);
-        })
-        setMessages(messages)
-    }, [context?.currentChannel?.channel_id])
 
-    console.log(context?.currentChannel?.channel_id)
+        fetch(`/api/messages?channel_id=${context?.currentChannel?.channel_id}`)
+            .then(async res => {
+                const json = await res.json();
+                setMessages(json);
+            })
+            .catch(error => {
+                console.error("Error fetching messages:", error);
+                setMessages([]); // Set messages to empty array in case of an error
+            });
+    }, [context?.currentChannel]);
 
     return (
         <div className={styles.messages}>
             {
-                messages && messages.map(
+                messages.length !== 0 ? messages.map(
                     (val) => <Message key={val.channel_id} message={val} />
-                )
+                ) : <p>Loading messages...</p>
             }
         </div >
     )
@@ -35,6 +37,18 @@ export default function Messages() {
 
 function Message({ message }: { message: CombinedMessage }) {
     return (
-        <p key={message.user.display_name || message.user.username}>{message.content}</p>
+        <div className={styles.container}>
+            <img className={styles.pfp} src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=identicon&f=y" alt="profile" />
+            <div>
+                <div className={styles.message_data}>
+                    <p>{message.user.display_name}</p>
+                    <p className={styles.message_date}>{message.created_at}</p>
+                </div>
+
+                <p className={styles.message_content}>
+                    {message.content}
+                </p>
+            </div>
+        </div>
     )
 }
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,13 +3,27 @@
 import styles from "./sidebar.module.css"
 import { useContext } from "react";
 import { AppContext } from "@/app/page";
+import { Channel } from "@/types/types";
 
 export default function Sidebar() {
     const context = useContext(AppContext);
 
+    const changeChannel = (channel: Channel) => {
+        if (channel.channel_id == context?.currentChannel?.channel_id) { return; }
+        console.log(context?.currentChannel)
+        context?.setCurrentChannel(channel);
+    }
+
     return (
         <div className={styles.sidebar}>
-            {context?.channels.map((val) => <p key={val.channel_id}>{val.channel_name} - {val.description}</p>)}
+            {context?.channels.map(val =>
+                <a
+                    role="button"
+                    onClick={() => changeChannel(val)}
+                    key={val.channel_id}>
+                    {val.channel_name} - {val.description}
+                </a>
+            )}
         </div >
     )
 }
